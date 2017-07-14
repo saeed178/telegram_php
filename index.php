152808754c479b05135cd7fc4e683f29e7ffabde
saeed178 <saeed7htc@gmail.com>
2017-07-14 03:58:28 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,102 +1,20 @@
-<?php
-require_once('lib/telegram.php');
+<?php 
+define('BOT_TOKEN', '447205620:AAGfcb3s9IXBeY28b3h0cP1zpxjXWh7Wavw');
+define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
+	
+// read incoming info and grab the chatID
+$content = file_get_contents("php://input");
+$update = json_decode($content, true);
+$chatID = $update["message"]["chat"]["id"];
+		
+// compose reply
+$reply =  sendMessage();
+		
+// send reply
+$sendto =API_URL."sendmessage?chat_id=".$chatID."&text=".$reply;
+file_get_contents($sendto);
 
-define('PATH', realpath('./'));
-$config = json_decode(file_get_contents(PATH."/config.json"), true);
-
-// telegram instance
-$telegram = new telegram($config['TELEGRAM_API_KEY']);
-
-// get Uri
-$uri = $_SERVER['REQUEST_URI'];
-
-switch($uri)
-{
-	case '/webHookUpdates':
-	{
-		$result = getRequest();
-		$telegram->parse($result);
-
-		break;
-	}
-	case '/getUpdates':
-	{
-		$offset = null;
-		$method = 'getUpdates';
-
-		$params = array
-		(
-			'offset' => $offset,
-			'limit' => null,
-			'timeout' => null
-		);
-
-		do
-		{
-			$result = $telegram->request($method, $params);
-
-			echo "<pre>";
-			print_r($result);
-			echo "</pre>";
-
-			// parse each comment
-			foreach($result['result'] as $r)
-			{
-				$params['offset'] = $r['update_id'];
-				$telegram->parse($r);
-			}
-
-			// to remove notifications, else always get the last
-			$params['offset']++;
-		}
-		while($result['ok']===true && count($result['result'])>0);
-		break;
-	}
-
-	case '/setWebhook':
-	{
-		$method = 'setWebhook';
-		$url = "https://".$config['APP_ID'].".herokuapp.com/webHookUpdates";
-		$params = array
-		(
-			'url' => $url
-		);
-		$result = $telegram->request($method, $params);
-
-		echo "<pre>";
-		print_r($result);
-		echo "</pre>";
-
-		break;
-	}
-
-	case '/unsetWebhook':
-	{
-                $method = 'setWebhook';
-                $params = array
-                (
-                        'url' => null
-                );
-                $result = $telegram->request($method, $params);
-
-                echo "<pre>";
-                print_r($result);
-                echo "</pre>";
-
-		break;
-	}
-
-	default:
-	{
-		include('view/default.tpl');
-	}
-}
-
-function getRequest()
-{
-	$postdata = file_get_contents("php://input");
-	$json = json_decode($postdata, true);
-	if($json)
-		return $json;
-	return $postdata;
+function sendMessage(){
+$message = "I am a baby bot.";
+return $message;
 }
